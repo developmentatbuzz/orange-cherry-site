78a821b5557a799514b23b9093d6286b704aad1d
nabil989 <73457486+nabil989@users.noreply.github.com>
2022-11-11 12:54:35 -0600

    up2date

---
+++ frontend/components/cherry.js
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline';
+
+const OrangeCherry = () => {
+    return (
+        <div className='bg-green-200 '>
+            <Spline className='h-1/3 w-1/3 bg-red-400' scene="https://prod.spline.design/JnV6cMWxcOxMY8Wf/scene.splinecode" />
+
+        </div>
+    );
+}
+ 
+export default OrangeCherry;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import OrangeCherry from '../components/cherry'
 export default function Home() {
   return (
-    <div>
+    <div className='bg-teal-100 h-screen w-screen'>
       <h1 className='text-7xl text-red-600'>hi</h1>
+      <h2>aa</h2>
+      {/* <OrangeCherry /> */}
+        <div className='transition-all duration-75 scale-[0.8] ' id="earth" >
+            {/* <div className='w-1/2 h-1/2 absolute'></div> */}
+            <OrangeCherry/>
+        </div>
     </div>
   )
 }
