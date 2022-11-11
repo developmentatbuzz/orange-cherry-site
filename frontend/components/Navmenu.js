bafe33cdc4c5bec23d3bdae9da5bb7c347a0f196
Donald Thai <donaldfromthailand73@gmail.com>
2022-11-11 01:14:17 -0600

    Added scroll to navbar

--- frontend/components/Navmenu.js
+++ frontend/components/Navmenu.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import $ from "jquery";
 import styles from "../styles/Nav.module.css";
 const Navmenu = ({ activeNav, setActiveNav }) => {
   const [style, setStyle] = useState({});
@@ -14,73 +15,79 @@ const Navmenu = ({ activeNav, setActiveNav }) => {
 
       <nav
         className={`${styles.navWrapper}`}
-        onMouseMove={(e) => {
-          setStyle({
-            "left":
-              ((window.innerWidth - window.outerWidth - window.outerWidth/7) * (e.pageX / window.innerWidth).toFixed(3)).toFixed(1) +
-              "px",
-          });
-          // cntWd = window.innerWidth;
-          // tb = $('#thumbs');
-          // sldWd = tb.outerWidth();
-        }}
-        style={style}
         >
         <div
-          className={`${styles.linkWrapper} ${
-            activeNav ? styles.linkWrapperOpen : ""
-          }`}
+          onMouseMove={(e) => {
+            // From: https://codepen.io/piogil/pen/XebobL jquery solution. 
+  
+            let cntWd = window.innerWidth;
+            let links = $('#links');
+            let sldWd = links.outerWidth();
+            links.css({left: ((cntWd - sldWd)*((e.pageX / cntWd).toFixed(3))).toFixed(1) +"px" });
+          }}
+          onMouseLeave={(e) => {
+            let links = $('#links');
+            links.css({left: "0px"});
+          }}
           id="links"
+          className={`${styles.navInnerWrapper}`}
           >
-          
-          <a
-            href=""
-            className={`${styles.navTab} ${
-              activeNav ? styles.navTabOpen : ""
-            }`}>
-            <h2 className={`${styles.navText}`}>Home</h2>
-            <img
-              src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt=""
-              className={`${styles.navImg}`}
-            />
-          </a>
-          <a
-            href=""
-            className={`${styles.navTab} ${
-              activeNav ? styles.navTabOpen : ""
-            }`}>
-            <h2 className={`${styles.navText}`}>Albums</h2>
-            <img
-              src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt=""
-              className={`${styles.navImg}`}
-            />
-          </a>
-          <a
-            href=""
-            className={`${styles.navTab} ${
-              activeNav ? styles.navTabOpen : ""
-            }`}>
-            <h2 className={`${styles.navText}`}>About Us</h2>
-            <img
-              src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt=""
-              className={`${styles.navImg}`}
-            />
-          </a>
-          <a
-            href=""
-            className={`${styles.navTab} ${
-              activeNav ? styles.navTabOpen : ""
-            }`}>
-            <h2 className={`${styles.navText}`}>Lmfao</h2>
-            <img
-              src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt=""
-              className={`${styles.navImg}`}
-            />
-          </a>
+
+          <div
+            className={`${styles.linkWrapper} ${
+              activeNav ? styles.linkWrapperOpen : ""
+            }`}
+            >
+            
+            <a
+              href=""
+              className={`${styles.navTab} ${
+                activeNav ? styles.navTabOpen : ""
+              }`}>
+              <h2 className={`${styles.navText}`}>Home</h2>
+              <img
+                src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className={`${styles.navImg}`}
+              />
+            </a>
+            <a
+              href=""
+              className={`${styles.navTab} ${
+                activeNav ? styles.navTabOpen : ""
+              }`}>
+              <h2 className={`${styles.navText}`}>Albums</h2>
+              <img
+                src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className={`${styles.navImg}`}
+              />
+            </a>
+            <a
+              href=""
+              className={`${styles.navTab} ${
+                activeNav ? styles.navTabOpen : ""
+              }`}>
+              <h2 className={`${styles.navText}`}>About Us</h2>
+              <img
+                src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className={`${styles.navImg}`}
+              />
+            </a>
+            <a
+              href=""
+              className={`${styles.navTab} ${
+                activeNav ? styles.navTabOpen : ""
+              }`}>
+              <h2 className={`${styles.navText}`}>Lmfao</h2>
+              <img
+                src="https://images.unsplash.com/photo-1667848015080-00ee6e487bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className={`${styles.navImg}`}
+              />
+            </a>
+          </div>
         </div>
       </nav>
     </>
